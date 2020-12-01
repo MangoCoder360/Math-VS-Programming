c6a19253c24b9adbf8367d3d9cbee61b251fd6b7
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-12-01 10:54:25 -0800

    Create variblesAndConstants.js

---
+++ variblesAndConstants.js
@@ -0,0 +1,21 @@
+// Varibles in programming:
+
+var myVarible = 10
+myVarible = 2
+myVarible = myVarible + 3
+
+// first myVarible = 10
+// then myVarible = 2
+// then varible = 5
+
+
+
+
+
+// Constants in programming:
+
+const myConstant = 10
+const newConst = myConstant - 2
+
+// myConstant = 10 and cannot be changed
+// newConst = 8 and cannot be changed
